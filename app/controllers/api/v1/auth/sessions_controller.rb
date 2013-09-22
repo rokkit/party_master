36221c750ddde9f81ba1d399799f60500f48ed95
Maxim Indykov <indmaksim@gmail.com>
2013-09-22 14:43:36 +0400

    add creating user when not exists in database

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -10,12 +10,26 @@ class Api::V1::Auth::SessionsController < Devise::SessionsController
       #set_flash_message(:notice, :signed_in) if is_navigational_format?
       #sign_in(resource_name, resource)
       if resource.nil?
-        render json: nil, status: 401
-      elsif resource.token = params[:token]
+        self.resource = User.create! username: params[:username],
+                                     token: params[:token], 
+                                     email: "#{params[:username]}@#{params[:provider]}.com",
+                                     password: "password", 
+                                     password_confirmation: "password" if check_user_on_provider params[:username], params[:provider]
+      end
+      if resource.token == params[:token]
         sign_in(resource_name, resource)
         resource.ensure_authentication_token!
         ssid = resource.authentication_token
         render json: { user: resource, ssid: ssid }, status: :created
       end
+  end
+  
+  private 
+  def check_user_on_provider username, provider
+    if true
+      true
+    else
+      render json: nil, status: 401
+    end
   end
 end
--- spec/requests/users_authentications_spec.rb
+++ spec/requests/users_authentications_spec.rb
@@ -8,12 +8,22 @@ describe "UsersAuthentications", api: true do
     let!(:user) { FactoryGirl.create :user }
 
     before(:each) do
-      post api_v1_auth_user_session_path(format: :json), token: token, username: username
+      post api_v1_auth_user_session_path(format: :json), token: token, username: username, provider: 'vk'
     end
 
     context "when user with given credentials not exist" do
-      let(:username) { "not_exist" }
-      it { last_response.status.should eq(401) }
+      
+      context "user valid on provider" do
+        let(:username) { "vasya" }
+        it { last_response.status.should eq(201) }
+        it "return ssid" do
+          data['ssid'].should_not be_nil
+        end
+      end
+      context "user not valid on provider" do
+        let(:username) { "not_exist" }
+        it "should return 401"
+      end
     end
 
     context "user with username exist but token expired but valid on provider" do
